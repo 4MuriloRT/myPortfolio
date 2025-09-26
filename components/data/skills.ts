84da594a612a41069a6950858e1c375be555627a
Murilo Taborda <murilotaborda09@gmail.com>
2025-09-26 17:03:17 -0300

    Finaliza parte de habilidades

--- components/data/skills.ts
+++ components/data/skills.ts
@@ -17,7 +17,7 @@ export const skills: Skills[] = [
         title: "JavaScript",
         description: "Linguagem de programação que adiciona interatividade às páginas.",
         src: "/imageskill/ImagemSkillJS.png",
-        group: "frontend",
+        group: "backend",
     },
     {
         title: "React",
@@ -35,7 +35,7 @@ export const skills: Skills[] = [
         title: "TypeScript",
         description: "Superset do JavaScript com tipagem estática.",
         src: "/imageskill/ImagemSkillTS.png",
-        group: "frontend",
+        group: "backend",
     },
     {
         title: "Sass",
@@ -60,5 +60,65 @@ export const skills: Skills[] = [
         description: "Framework que simplifica o desenvolvimento de apps React Native.",
         src: "/imageskill/ImagemSkillExpo.png",
         group: "other",
+    },
+    {
+        title: "Figma",
+        description: "Ferramenta de design colaborativo para interfaces e protótipos.",
+        src: "/imageskill/ImagemSkillFigma.png",
+        group: "other",
+    },
+    {
+        title: "Git",
+        description: "Sistema de controle de versão para gerenciamento de código.",
+        src: "/imageskill/ImagemSkillGit.png",
+        group: "other",
+    },
+    {
+        title: "TailwindCss",
+        description: "Framework CSS utilitário que permite estilização rápida e consistente de interfaces modernas.",
+        src: "/imageskill/ImagemSkillTW.png",
+        group: "frontend",
+    },
+    {
+        title: "Next.js",
+        description: "Framework React para aplicações web modernas e escaláveis.",
+        src: "/imageskill/ImagemSkillNext.png",
+        group: "frontend",
+    },
+    {
+        title: "AWS",
+        description: "Plataforma de serviços em nuvem para soluções escaláveis.",
+        src: "/imageskill/ImagemSkillAWS.png",
+        group: "backend",
+    },
+    {
+        title: "Trabalho em equipe",
+        description: "Colaboração eficaz para alcançar objetivos comuns.",
+        src: "/imageskill/ImagemSkillTrabalhoEquipe.png",
+        group: "corporate",
+    },
+    {
+        title: "Compromisso",
+        description: "Dedicação no cumprimento de prazos e metas.",
+        src: "/imageskill/ImagemSkillComp.png",
+        group: "corporate",
+    },
+    {
+        title: "Flexibilidade",
+        description: "Capacidade de adaptação a mudanças e novos desafios.",
+        src: "/imageskill/ImagemSkillFlex.png",
+        group: "corporate",
+    },
+    {
+        title: "Comunicação",
+        description: "Transmissão clara e objetiva de ideias e informações.",
+        src: "/imageskill/ImagemSkillSpeak.png",
+        group: "corporate",
+    },
+    {
+        title: "Pontualidade",
+        description: "Cumprimento rigoroso de prazos e horários.",
+        src: "/imageskill/ImagemSkillTime.png",
+        group: "corporate",
     },
 ]
